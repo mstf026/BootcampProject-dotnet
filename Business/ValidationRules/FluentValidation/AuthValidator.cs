19930cdc8816be09e794cfe7c5a824b4bc94d9b1
mstf026 <mstfylmz10@outlook.com>
2023-07-30 18:01:09 +0300

    30 temmuz 2

---
+++ Business/ValidationRules/FluentValidation/AuthValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.DTOs;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class AuthValidator : AbstractValidator<UserForRegisterDto>
+    {
+        public AuthValidator()
+        {
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Email).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty();
+
+        }
+
+        //private bool StartWithA(string arg)
+        //{
+        //    return arg.StartsWith("A");
+        //}
+    }
+}
